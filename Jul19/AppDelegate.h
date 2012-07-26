e0d88cd16a33a88e9d0fc134ac93b57f15ad89f4
wh33landaxel <xlnunez317@gmail.com>
2012-07-26 13:20:09 -0400

    Initial Commit

---
+++ Jul19/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jul19
+//
+//  Created by Axel Nunez on 7/26/12.
+//  Copyright (c) 2012 CUNY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
